f8f002b75434436f891c91b5ee937140c9e0cbce
dong <dho0647@naver.com>
2026-01-05 14:46:25 +0900

    간단한 채팅 구현v2

--- src/main/java/com/mediazen/chat/controller/RoomController.java
+++ src/main/java/com/mediazen/chat/controller/RoomController.java
@@ -1,12 +1,10 @@
 package com.mediazen.chat.controller;
 
 import com.mediazen.chat.domain.Room;
+import com.mediazen.chat.domain.RoomRequest;
 import com.mediazen.chat.service.RoomService;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
 
@@ -18,8 +16,8 @@ public class RoomController {
     private final RoomService roomService;
 
     @PostMapping
-    public Mono<Room> createRoom() {
-        return roomService.createRoom("sobin");
+    public Mono<Room> createRoom(@RequestBody RoomRequest request) {
+        return roomService.createRoom(request.name());
     }
 
     @GetMapping
---
+++ src/main/java/com/mediazen/chat/domain/RoomRequest.java
@@ -0,0 +1,6 @@
+package com.mediazen.chat.domain;
+
+public record RoomRequest(
+        String name
+) {
+}
